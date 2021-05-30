3c8783ee3e6d7fca4a7564899651d72772cf9757
Vinícius Romano <mdcdxcvi@gmail.com>
2021-05-30 00:35:39 -0300

    First commit

---
+++ cs_py_bot/_tests/test_extract_matches.py
@@ -0,0 +1,5 @@
+from cs_py_bot.extract_matches import Extract
+
+
+def test_check_files():
+    pass
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="CS Bot",
+    version="1.0.0",
+    description="A bot to bat in CSGO matches.",
+    author="romavini",
+    packages=["cs_py_bot"],
+)
